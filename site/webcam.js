0c39fe3173dce8d1e7530a16e837d426e8481502
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-12 01:03:24 +0100

    webcam rotation

--- site/webcam.js
+++ site/webcam.js
@@ -19,29 +19,17 @@
   var photo = null;
   var startbutton = null;
 
-  function showViewLiveResultButton() {
-    if (window.self !== window.top) {
-      // Ensure that if our document is in a frame, we get the user
-      // to first open it in its own tab or window. Otherwise, it
-      // won't be able to request permission for camera access.
-      document.querySelector(".contentarea").remove();
-      const button = document.createElement("button");
-      button.textContent = "View live result of the example code above";
-      document.body.append(button);
-      button.addEventListener('click', () => window.open(location.href));
-      return true;
-    }
-    return false;
-  }
-
   function startup() {
-    if (showViewLiveResultButton()) { return; }
     video = document.getElementById('video');
     canvas = document.getElementById('canvas');
     photo = document.getElementById('photo');
     startbutton = document.getElementById('startbutton');
 
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    navigator.mediaDevices.getUserMedia(
+      {
+        video: {width: {exact: 1280}, height: {exact: 720}}
+      }
+    )
     .then(function(stream) {
       video.srcObject = stream;
       video.play();
@@ -98,10 +86,12 @@
   function takepicture() {
     var context = canvas.getContext('2d');
     if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-
+      canvas.width = video.height;
+      canvas.height = video.width;
+      context.translate(canvas.width * 0.5, canvas.height * 0.5);
+      context.rotate(Math.PI/180*90);
+      context.translate(-video.width * 0.5, -video.height * 0.5);
+      context.drawImage(video, 0,0,width,height);      
       var data = canvas.toDataURL('image/png');
       photo.setAttribute('src', data);
     } else {
