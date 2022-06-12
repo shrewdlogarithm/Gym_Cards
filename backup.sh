a8c3589e9c2b00559c65d726fde74ce649d87107
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-12 12:01:38 +0100

    Backup folder

--- backup.sh
+++
@@ -1,8 +0,0 @@
-now=$(date +"%Y_%m_%d")
-tar -cvf "backup.$now.tar" data/* logs/*
-#ncftpput -u gym@theedgeofthevoid.co.uk -p 'LeeLee23"£' ftp.theedgeofthevoid.co.uk . "backup.$now.tar"
-
-if [ ! -f ~/Gym_Cards/active ]
-then
-	echo "SHUTDOWN STARTING" > ~/Gym_Cards/logs/system.log 2> &1
-fi
