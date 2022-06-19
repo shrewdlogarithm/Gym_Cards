fa23b513c51a31ca285d0397a7232337b448b80e
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-19 15:40:23 +0100

    Pre-Update Backup

--- system/update.sh
+++ system/update.sh
@@ -1,1 +1,3 @@
+var dt = $(date +"\%Y_\%m_\%d_\%H")
+tar cvzf preupdate$dt ../Gym_Cards/*
 git pull https://shrewdlogarithm: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@github.com/shrewdlogarithm/Gym_Cards.git
