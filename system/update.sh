bfe0ed24d597c23f1123f815113b59f711c1cc3d
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-19 15:43:37 +0100

    Fix Name

--- system/update.sh
+++ system/update.sh
@@ -1,3 +1,3 @@
-dt=$(date +"\%Y_\%m_\%d_\%H")
+dt=$(date +"%Y_%m_%d")
 tar cvzf preupdate$dt.gz ../Gym_Cards/*
 git pull https://shrewdlogarithm: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@github.com/shrewdlogarithm/Gym_Cards.git
