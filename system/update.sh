d33205a5d004e635442790a5ea2b7bae1f54529d
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-23 22:31:59 +0100

    Update backup now includes hidden files

--- system/update.sh
+++ system/update.sh
@@ -1,5 +1,5 @@
 cd ~
 dt=$(date +"%Y_%m_%d")
-tar cvzf preupdate$dt.gz Gym_Cards/*
+tar cvzf preupdate$dt.gz Gym_Cards/.*
 cd Gym_Cards
 git pull https://shrewdlogarithm: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@github.com/shrewdlogarithm/Gym_Cards.git
