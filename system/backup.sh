3a7abfb15cb5bc454a9b2285132f680f25e655b0
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-24 23:28:23 +0100

    Backup settings and live database

--- system/backup.sh
+++ system/backup.sh
@@ -3,8 +3,7 @@ date
 
 cd ~/Gym_Cards
 
-# We don't backup the current database to avoid collisions
-tar cvzf "backups/backup$1.tar.gz" data/*.bak images/* logs/*
+tar cvzf "backups/backup$1.tar.gz" data/* images/* logs/*
 
 ncftpput -u gym@theedgeofthevoid.co.uk -p 'LeeLee23"£' ftp.theedgeofthevoid.co.uk . ~/Gym_Cards/backups/*.gz
 if [ $? -eq 0 ]
