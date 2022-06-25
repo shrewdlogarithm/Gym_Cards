b9f44995e1b3aa074bf1e124ffc5a78ef2f9d651
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-25 19:40:11 +0100

    Install/Update improvements

--- system/install.sh
+++ system/install.sh
@@ -3,26 +3,21 @@
 
 #git clone https://shrewdlogarithm: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@github.com/shrewdlogarithm/Gym_Cards.git
 
+sudo apt-get update
+sudo apt-get upgrade
+
 cd ~/Gym_Cards/site
 bash get_tablesorter.sh
 cd
 
+pip3 install python_dateutil
 sudo apt install python3-gst-1.0
 pip3 install playsound
-
 pip3 install evdev
-
-pip3 install python_dateutil
 
 sudo apt-get install ncftp
 
-cd ~/Gym_Cards/system
-cp -R autostart ~/.config/
-cp -R lxsession/* ~/config/
-cat crontab | crontab -u admin -
-cd
-
 curl https://download.argon40.com/argon1.sh | bash
 
-sudo apt-get update
-sudo apt-get upgrade
+cd~/Gym_Cards/system
+bash update.sh
--- system/update.sh
+++ system/update.sh
@@ -1,5 +1,12 @@
-cd ~
+cd
 dt=$(date +"%Y_%m_%d")
 tar cvzf preupdate$dt.gz Gym_Cards/.*
 cd Gym_Cards
 git pull https://shrewdlogarithm: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@github.com/shrewdlogarithm/Gym_Cards.git
+
+cd system
+cp -R autostart ~/.config/
+cp -R lxsession/* ~/.config/
+
+cat crontab | crontab -u admin -
+cd
