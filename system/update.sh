955e079d9746d24d86f4ae0c0b247cb76e6deff3
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-06-27 10:35:59 +0100

    Update backup path

--- system/update.sh
+++ system/update.sh
@@ -1,6 +1,6 @@
 cd ~/Gym_Cards
 dt=$(date +"%Y_%m_%d")
-tar cvzf preupdate$dt.gz .
+tar cvzf ../preupdate$dt.gz .
 
 git pull https://shrewdlogarithm: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@github.com/shrewdlogarithm/Gym_Cards.git
 
