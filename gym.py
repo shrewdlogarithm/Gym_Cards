69cfce157da18c2a228d999a1331f4c986992413
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-07-06 00:56:33 +0100

    No-Cache doesn't work - removed

--- gym.py
+++ gym.py
@@ -564,14 +564,6 @@ def stream():
             yield msg
     return Response(stream(), mimetype='text/event-stream')
 
-@app.after_request
-def add_header(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-    
 @app.context_processor
 def handle_context():
     return dict(os=os)
