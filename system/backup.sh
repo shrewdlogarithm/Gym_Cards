9da55dd7bb3c2fbfdfd3a62b3f5c83cd025a5002
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-07-09 00:23:05 +0100

    Backup changes (images)

--- system/backup.sh
+++ system/backup.sh
@@ -3,7 +3,13 @@ date
 
 cd ~/Gym_Cards
 
-tar cvzf backups/$HOSTNAME-backup-$1.tar.gz data/* site/images/* logs/*
+tar cvzf backups/$HOSTNAME-backup-$1.tar.gz data/* logs/*
+tar cvzf backups/$HOSTNAME-images.tar.gz site/images/*
+
+if [ -z $1 ]
+then
+        $1 = ""
+fi
 
 ncftpput -u gym@theedgeofthevoid.co.uk -p 'LeeLee23"£' ftp.theedgeofthevoid.co.uk . ~/Gym_Cards/backups/*.gz
 if [ $? -eq 0 ]
@@ -11,13 +17,9 @@ then
         rm backups/*.gz
 fi
 
-if [ -z $1 ]
-then
-        echo "Backup Complete"
-else
-        if [ $1 = "shutdown" ]
-        then    
-                echo "Shutdown Commencing"
-                sudo shutdown now
-        fi
+echo "Backup Complete"
+if [ $1 = "shutdown" ]
+then    
+        echo "Shutdown Commencing"
+        sudo shutdown now
 fi
