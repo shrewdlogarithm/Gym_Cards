5398411761719c80dc9dc855b50ffd47752df09d
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-07-08 15:46:14 +0100

    backup file name

--- system/backup.sh
+++ system/backup.sh
@@ -1,9 +1,11 @@
 echo "Backup running"
 date
 
+now=$(date +"\%Y_\%m_\%d_\%H")
+
 cd ~/Gym_Cards
 
-tar cvzf "backups/$HOSTNAME-backup-$(date +"\%Y_\%m_\%d_\%H").tar.gz" data/* images/* logs/*
+tar cvzf backups/$HOSTNAME-backup-$now.tar.gz data/* images/* logs/*
 
 ncftpput -u gym@theedgeofthevoid.co.uk -p 'LeeLee23"£' ftp.theedgeofthevoid.co.uk . ~/Gym_Cards/backups/*.gz
 if [ $? -eq 0 ]
