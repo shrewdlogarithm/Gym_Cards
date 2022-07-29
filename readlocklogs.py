b509bbac73e1bf783c62f87dd4ba81f104099ed9
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2022-07-30 00:20:11 +0100

    More lock testing

--- readlocklogs.py
+++ readlocklogs.py
@@ -1,16 +1,17 @@
 from pyquery import PyQuery
 import re,time,requests
-lock = 'http://192.168.1.143'
+lock = 'http://127.0.0.1'
+#lock = 'http://192.168.1.143'
 
 def getpage(path,vars={}):
-    return requests.post(lock + "/" + path, headers={'Content-Type': 'application/x-www-form-urlencoded','referer': "192.168.1.143"}, data = vars, timeout=1).text
+    return requests.post(lock + "/" + path, headers={'Content-Type': 'application/x-www-form-urlencoded','referer': "192.168.1.143"}, data = vars, timeout=1)
 
 un = 1
 page = getpage("ACT_ID_21",{'s4': 'Swipe'})
 while 1==1:
     recid2 = 0
-    pgs = re.findall(r"Page[^0-9]+?([0-9]+)[^0-9]+?Of[^0-9]+?([0-9]+)[^0-9]+?Page",page)
-    pq = PyQuery(bytes(bytearray(page, encoding='utf-8')))
+    pgs = re.findall(r"Page[^0-9]+?([0-9]+)[^0-9]+?Of[^0-9]+?([0-9]+)[^0-9]+?Page",page.text)
+    pq = PyQuery(bytes(bytearray(page.text, encoding='utf-8')))
     for row in pq("table:last tr"):
         cells = []
         for cell in PyQuery(row)("td"):
@@ -25,6 +26,8 @@ while 1==1:
         while 1==1:
             try:
                 print("Trying to read page ", un)
+                if page:
+                    page.close()
                 page = getpage("ACT_ID_345",{
                     "PC":recid2,
                     "PE":"0",
@@ -34,4 +37,4 @@ while 1==1:
             except Exception as e:
                 print("Failed with ", e)
                 time.sleep(1)
-    time.sleep(3)
+    time.sleep(0)
