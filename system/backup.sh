0f5f9e5528a5252c02718566e0719ffe50600180
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2023-01-07 12:16:29 +0000

    Add hostname to backup msg to check it's not blank

--- system/backup.sh
+++ system/backup.sh
@@ -1,10 +1,10 @@
-echo "Backup running"
+host=$HOSTNAME
+echo "Backup of $(host) running"
 date
 
 echo "DDNS Update:"
 echo url="https://api.dynu.com/nic/update?hostname=gymlee.mywire.org&password=leelee23&myipv4=$(curl -s -4 ifconfig.co)&myipv6=$(curl -s -6 ifconfig.co)" | curl -k -K  -
 
-host=$HOSTNAME
 opt=$1
 if [ -z $1 ]
 then
@@ -13,11 +13,11 @@ fi
 
 cd ~/Gym_Cards
 
-tar czf backups/${host}-backup-$opt.tar.gz data/* logs/*
+tar czf backups/${host}-backup-$opt.tar.gz data/*
 
 #ncftpput -u gym@theedgeofthevoid.co.uk -p 'LeeLee23"£' ftp.theedgeofthevoid.co.uk . ~/Gym_Cards/backups/*.gz
 
-lftp -u 'gym@theedgeofthevoid.co.uk','LeeLee23"£' ftp://ftp.theedgeofthevoid.co.uk:21 -e "set ftp:ssl-force true;set ftp:ssl-protect-data true;set ssl:verify-certificate no;set sftp:auto-confirm yes;mput backups/*.gz;mirror -R -n site/images ${host}images;exit"
+lftp -u 'gym@theedgeofthevoid.co.uk','LeeLee23"£' ftp://ftp.theedgeofthevoid.co.uk:21 -e "set ftp:ssl-force true;set ftp:ssl-protect-data true;set ssl:verify-certificate no;set sftp:auto-confirm yes;mput backups/*.gz;mirror -R -n site/images ${host}images;mirror -R -n site/logs ${host}logs;exit"
 
 if [ $? -eq 0 ]
 then
