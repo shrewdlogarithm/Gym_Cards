8e9ca3652f88e3a08cae348f321ed1594e088818
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2023-05-07 00:26:02 +0100

    Remove PI specific startup files and update backup

--- system/update.sh
+++ system/update.sh
@@ -1,12 +1,7 @@
 cd ~/Gym_Cards
-dt=$(date +"%Y_%m_%d-%H:%M:%S")
-tar cvzf ../preupdate$dt.gz .
 
 git pull https://github.com/shrewdlogarithm/Gym_Cards.git
 
-cd system
-cp -R autostart ~/.config/
-cp -R lxsession/* ~/.config/
-
 cat crontab | crontab -u $USER -
+
 cd
