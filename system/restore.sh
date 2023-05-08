b274b90b4e7b796e2ea7312e3a832bbf6f61e68b
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2023-05-08 13:32:59 +0100

    Force new restore

--- system/restore.sh
+++ system/restore.sh
@@ -1,8 +1,8 @@
 host=$1
 if [ -z $1 ]
 then
-    echo "Requires HOSTNAME as parameter"
+    echo "Requires HOSTNAME"
 else
-    cd ~/Gym_Cards
+    cd ~/Gym_Cards 
     lftp -u 'gym@theedgeofthevoid.co.uk','LeeLee23"£' ftp://ftp.theedgeofthevoid.co.uk:21 -e "set ftp:ssl-force true;set ftp:ssl-protect-data true;set ssl:verify-certificate no;set sftp:auto-confirm yes;mirror -n ${host}images site/images;mirror -n ${host}logs logs ;mirror -n ${host}data data ;exit"
 fi
