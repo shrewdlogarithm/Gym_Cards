d5a323379cf3c8311b28cb603efcc942d00413bd
John Peat <johnpeat@yahoo.com>
2023-05-16 19:09:35 +0100

    Run Fix

--- system/run.sh
+++ system/run.sh
@@ -1,4 +1,1 @@
-while :
-do
-    gnome-terminal -- bash -c "cd ~/Gym_Cards;python3 gym.py > logs/system.log 2>&1"
-done
+gnome-terminal -- bash -c "cd ~/Gym_Cards;python3 gym.py > logs/system.log 2>&1"
