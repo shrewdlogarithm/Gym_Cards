f7dc38327903b22caaee835d75ea0f64439b3398
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2023-05-27 11:12:52 +0100

    Update restore

--- system/restore.sh
+++ system/restore.sh
@@ -4,6 +4,5 @@ then
     echo "Requires HOSTNAME"
 else
     cd ~/Gym_Cards 
-    lftp -u 'gym@theedgeofthevoid.co.uk','LeeLee23"£' ftp://ftp.theedgeofthevoid.co.uk:21 -e "set ftp:ssl-force true;set ftp:ssl-protect-data true;set ssl:verify-certificate no;set sftp:auto-confirm yes;mirror -n ${host}images site/images;mirror -n ${host}logs logs ;mirror -n ${host}data data ;exit"
-    python3 logdbload.py
+    lftp -u 'gym@theedgeofthevoid.co.uk','LeeLee23"£' ftp://ftp.theedgeofthevoid.co.uk:21 -e "set ftp:ssl-force true;set ftp:ssl-protect-data true;set ssl:verify-certificate no;set sftp:auto-confirm yes;mirror -n ${host}images site/images;mirror -n ${host}logs logs ;mirror -n ${host}data data ;exit"    
 fi
